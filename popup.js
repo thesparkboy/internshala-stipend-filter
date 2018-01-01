0a9813267c28e16a1f7d197c6298935a176e5da8
thesparkboy <madan.aakash2@gmail.com>
2018-01-01 12:46:53 +0530

    reset functionality added

--- popup.js
+++ popup.js
@@ -3,9 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
       'click', onClk);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#reset').addEventListener(
+      'click', reset);
+});
+
 function onClk(){
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	  chrome.tabs.sendMessage(tabs[0].id, {stipend: document.getElementById("stpnd").value}, function(response) {
+	    console.log(response.farewell);
+	  });
+	});
+}
+
+function reset(){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {stipend: 0}, function(response) {
 	    console.log(response.farewell);
 	  });
 	});
--- script.js
+++ script.js
@@ -2,22 +2,26 @@ var minValue = 0;
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     minValue = parseInt(request.stipend);
-	console.log(minValue);
+	console.log(request);
 	var data = document.getElementsByClassName("individual_internship");
-	for(var i = 1;i < data.length; i++) {
-		//data[i].style.display = "none";	
-		console.log(data[i]);
-		var stipend = data[i].getElementsByClassName("stipend_container_table_cell")[0].innerText.split(" ")[0];
-		if(stipend.indexOf("-") != -1){
-			stipend = stipend.split("-")[0];
+	if(minValue == 0){
+		for(var i = 0;i < data.length; i++) {
+			var stipend = data[i].getElementsByClassName("stipend_container_table_cell")[0].innerText.split(" ")[0];
+			data[i].style.display = "inline";
 		}
-		if(parseInt(stipend) < minValue){
-			data[i].style.display = "none";
+	}else{
+		for(var i = 0;i < data.length; i++) {
+			var stipend = data[i].getElementsByClassName("stipend_container_table_cell")[0].innerText.split(" ")[0];
+			if(stipend.indexOf("-") != -1){
+				stipend = stipend.split("-")[0];
+			}
+			if(parseInt(stipend) < minValue){
+				data[i].style.display = "none";
+			}
 		}
-		console.log(stipend);
-	}
-    if (request.stipend == "hello")
-      sendResponse({farewell: "goodbye"});
+	    if (request.stipend == "hello")
+	      sendResponse({farewell: "goodbye"});
+		}
   });
 /*var minValue = 4000;
 console.log('11111');
